637a4eb6bee1fc301379a587c1fed73531e20208
nam <namhoangpham96@gmail.com>
2022-08-26 18:15:54 +0700

    Create a new app

---
+++ src/z_framework/excel_folder/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ExcelFolderConfig(AppConfig):
+    name = 'excel_folder'
--- src/z_framework/z_framework/settings.py
+++ src/z_framework/z_framework/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Installed app
+    'excel_folder',
 ]
 
 MIDDLEWARE = [
