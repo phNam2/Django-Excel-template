d0f7a213b1d4806d506861c5dbf28a73ce8d87d0
nam <namhoangpham96@gmail.com>
2022-09-02 08:23:59 +0700

    Hide the secret_key

--- src/z_framework/z_framework/settings.py
+++ src/z_framework/z_framework/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'yhm3lrtwp_puby*w5x)$36l#v8@fcq^2c%023@*iiml300-j60'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
